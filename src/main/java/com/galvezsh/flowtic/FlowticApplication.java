e7cbf24f209e3a36dde3ffe683f4bad908ed977a
Galvesh <galvezssr@gmail.com>
2025-08-31 14:03:10 +0200

    Initial commit

---
+++ src/main/java/com/galvezsh/flowtic/FlowticApplication.java
@@ -0,0 +1,13 @@
+package com.galvezsh.flowtic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvezsh/flowtic/FlowticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvezsh.flowtic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
